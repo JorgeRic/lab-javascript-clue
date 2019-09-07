df56174a993ab89ba78e8500ed149ebe97e90951
Jorge <jorrillop@hotmail.com>
2019-09-07 19:54:55 +0200

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,94 +1,117 @@
-// Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
+'use strict'
 
-drOrchid
-first_name:   Doctor
-last_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-occupation:    Scientist
+//Iteration 1 - Creating the cards
 
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionaire video game designer
-age:          22
-image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation:   Designer
+const charactersArray  = [
+{
+name:         'mrGreen',
+first_name:   'Jacob',
+last_name:    'Green',
+color:        'green',
+description:  'He has a lot of connections',
+age:          45,
+image:        'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+occupation:   'Entrepreneur'
+},
+{
+name:         'drOrchid',
+first_name:   'Doctor',
+last_name:    'Orchid',
+color:        'white',
+description:  'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+age:          26,
+image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+occupation:    'Scientist'
+},
+{
+name:         'profPlum',
+first_name:   'Victor',
+last_name:    'Plum',
+color:        'purple',
+description:  'Billionaire video game designer',
+age:          22,
+image:        'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+occupation:   'Designer'
+},
+{
+name:         'missScarlet',
+first_name:   'Kasandra',
+last_name:    'Scarlet',
+color:        'red',
+description:  'She is an A-list movie star with a dark past',
+age:          31,
+image:        'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
+occupation:   'Actor'
+},
+{
+name:         'mrsPeacock',
+first_name:   'Eleanor',
+last_name:    'Peacock',
+color:        'blue',
+description:  'She is from a wealthy family and uses her status and money to earn popularity',
+age:          36,
+image:        'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+occupation:   'Socialité'
+},
+{
+name:         'mrMustard',
+first_name:   'Jack',
+last_name:    'Mustard',
+color:        'yellow',
+description:  'He is a former football player who tries to get by on his former glory',
+age:          62,
+image:        'https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg',
+occupation:   'Retired Football player'
+}
+];
 
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation:   Actor
+const weaponsArray = [
+ 'rope', 
+ 'knife', 
+ 'candlestick',
+ 'dumbbell',
+ 'poison',
+ 'axe',
+ 'bat',
+ 'trophy',
+ 'pistol',
+ ];
 
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
+ const roomsArray = [
+ 'Dining Room',
+ 'Conservatory',
+ 'Kitchen',
+ 'Study',
+ 'Library',
+ 'Billiard Room',
+ 'Lounge',
+ 'Ballroom',
+ 'Hall',
+ 'Spa',
+ 'Living Room',
+ 'Observatory',
+'Theater',
+ 'Guest House',
+ 'Patio',
+  ];
 
-mrMustard
-first_name:   Jack
-last_name:    Mustard
-color:        yellow
-description:  He is a former football player who tries to get by on his former glory
-age:          62
-image:        https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-occupation:   Retired Football player
 
-// Weapons
+function randomSelector (array){
+  return  array[Math.floor(Math.random() * array.length)]
+}
+console.log(randomSelector(charactersArray))
 
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
+var objetoMistery = []
+var pickMystery = function() {
+objetoMistery.push(randomSelector(charactersArray))
+objetoMistery.push(randomSelector(weaponsArray))
+objetoMistery.push(randomSelector(roomsArray))
+  return objetoMistery
+}
+console.log(pickMystery())
 
-// Rooms
-name: Dining Room
-name: Conservatory
-name: Kitchen
-name: Study
-name: Library
-name: Billiard Room
-name: Lounge
-name: Ballroom
-name: Hall
-name: Spa
-name: Living Room
-name: Observatory
-name: Theater
-name: Guest House
-name: Patio
-*/
+function revelarMisterio(){
+  console.log(`${objetoMistery[0].name} ${objetoMistery[0].first_name} killed Mr. Boddy using the ${objetoMistery[1]} in the ${objetoMistery[2]}!!!`)
+}
 
-// Characters Collection
-const charactersArray = [];
-
-// Rooms Collection
-const roomsArray = [];
-
-// Weapons Collection
-const weaponsArray = [];
+revelarMisterio()
